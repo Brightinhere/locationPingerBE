b97e46db0a902edbe3a91aba9fe2ac5daddd3579
Automated Heroku Deploy <irfan.bilir@hva.nl>
2022-10-04 14:19:25 +0200

    Gitignore & Db call

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express()
 const PORT = process.env.PORT || 80
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 app.use(express.json())
 app.listen(
@@ -15,6 +25,16 @@ app.get('/pong', (req, res) => {
   })
 })
 
-app.post('/ping', (req, res) => {
+app.post('/ping', async (req, res) => {
+
+  const fres = await db.collection('cities').add({
+    location: req.body.location,
+    user: req.body.user
+  });
+
+  res.status(200).send({
+    id: fres.id
+  })
+
     console.log(req.body);
 })
