7d47c93d58d167da3d82d57be1d668f1cd9c8d24
Automated Heroku Deploy <irfan.bilir@hva.nl>
2022-10-04 13:37:47 +0200

    Iets

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express()
 const PORT = process.env.PORT || 80
 
-
 app.use(express.json())
 app.listen(
   PORT,
   () => console.log(`Its alive on https://locationpicker.herokuapp.com/:${PORT}`)
 );
-
 
 app.get('/pong', (req, res) => {
   res.status(200).send({
